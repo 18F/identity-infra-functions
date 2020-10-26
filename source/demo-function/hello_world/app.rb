fa154229da92631a517a104f229e9c33ba3b4182
Brian Crissup <brian@BigMac.local>
2020-10-26 10:55:57 -0400

    Update config

--- source/demo-function/hello_world/app.rb
+++ source/demo-function/hello_world/app.rb
@@ -1,37 +1,7 @@
-require 'aws-sdk-s3'
 require 'json'
-require 'logger'
 
 def lambda_handler(event:, context:)
-  # Sample pure Lambda function
-
-  # Parameters
-  # ----------
-  # event: Hash, required
-  
-  # context: object, required
-  #     Lambda Context runtime methods and attributes
-  #     Context doc: https://docs.aws.amazon.com/lambda/latest/dg/ruby-context.html
-
-  # Returns
-  # ------
-  logger = Logger.new(STDOUT)
-
-  logger.info(event)
-  
-  s3_bucket_name = ENV["S3_BUCKET_NAME"]
-  environment = ENV["ENVIRONMENT_NAME"]
-  region = ENV["AWS_REGION"]
-  kms_key_alias = ENV["KMS_KEY_ALIAS"]
-
-  s3 = Aws::S3::Resource.new(region: region)
-
-  bucket = s3.bucket(s3_bucket_name)
-
-  bucket.objects.limit(50).each do |item|
-    logger.info(item)
-    print "Foo: #{item.key}"
-  end
-  return
+    # TODO implement
+    { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
 end
 
