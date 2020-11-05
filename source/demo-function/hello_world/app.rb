7436acaccb5164229439fecc40c95434867d5cec
Brian Crissup <brian@BigMac.local>
2020-11-05 11:41:35 -0500

    Add xray

--- source/demo-function/hello_world/app.rb
+++ source/demo-function/hello_world/app.rb
@@ -1,7 +1,8 @@
 require 'json'
+require 'aws-sdk-sns'
+require 'aws-xray-sdk/lambda'
 
 def lambda_handler(event:, context:)
     # TODO implement
     { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
 end
-
