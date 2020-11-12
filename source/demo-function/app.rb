4af254656ca4e56d2c628a53d8c237b2aad317a2
Brian Crissup <brian@BigMac.local>
2020-11-12 13:48:43 -0500

    Update code

--- source/demo-function/app.rb
+++ source/demo-function/app.rb
@@ -1,8 +1,12 @@
 require 'json'
 require 'aws-sdk-sns'
-require 'aws-xray-sdk/lambda'
+require 'aws-xray-sdk-core'
+
+sns = Aws::SNS::Resource.new()
 
 def lambda_handler(event:, context:)
+    topic = sns.topic('arn:aws:sns:us-west-2:894947205914:slack-otherevents')
+    topic.publish({message: 'Testing please ignoe'})
     # TODO implement
     { statusCode: 200, body: JSON.generate('Hello from Lambda!') }
 end
