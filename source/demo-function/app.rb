b29f8032667689a5e70bc62f2431241502e2d7aa
Brian Crissup <brian@BigMac.local>
2020-11-13 10:10:27 -0500

    Update sdk

--- source/demo-function/app.rb
+++ source/demo-function/app.rb
@@ -1,11 +1,18 @@
 require 'json'
 require 'aws-sdk-lambda'
 require 'aws-sdk-sns'
-require 'aws-sdk-xray'
+require 'aws-xray-sdk'
 
 $lambda = Aws::Lambda::Client.new()
 $lambda.get_account_settings()
-$sns = Aws::SNS:Resource.new()
+$sns = Aws::SNS::Resource.new()
+
+config = {
+  name: 'my app',
+  patch: %I[net_http aws_sdk]
+}
+
+XRay.recorder.configure(config)
 
 def lambda_handler(event:, context:)
     topic = $sns.topic('arn:aws:sns:us-west-2:894947205914:slack-otherevents')
